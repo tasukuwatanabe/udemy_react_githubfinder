fe3e6895a64b037fd6d79844f3ef763d8f82b1dd
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-02-18 23:46:05 +0900

    add alert component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Alert from './components/layout/Alert';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
@@ -15,6 +16,7 @@ function App() {
           <div className='flex flex-col justify-between h-screen'>
             <Navbar />
             <main className='container mx-auto px-3 pb-12'>
+              <Alert />
               <Routes>
                 <Route exact path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />

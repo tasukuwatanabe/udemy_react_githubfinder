f878225ecaecfd33b56ef8726fdf5c1d605690bd
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-02-19 14:59:00 +0900

    get single user

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Alert from './components/layout/Alert';
+import User from './pages/User';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
@@ -20,6 +21,7 @@ function App() {
               <Routes>
                 <Route exact path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
                 <Route path='/notfound' element={<NotFound />} />
                 <Route path='/*' element={<NotFound />} />
               </Routes>
